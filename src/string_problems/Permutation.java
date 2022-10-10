e8e3cbbb521758b04ca9ccd8565ecddd776b10cf
Fouad-Mesrane <fouadmesrane@gmail.com>
2022-10-10 16:31:39 -0600

    10/10/22 string_problems.Permutation solved

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -9,7 +9,25 @@ public class Permutation {
      */
 
     public static void main(String[] args) {
+        permutation("abb");
 
     }
     // Implement Here
+    public static void permutation(String str){
+       permutation("", str);
+
+    }
+    public static void permutation(String prefix, String str){
+        if (str.length() == 0){
+            System.out.println(prefix);
+            return;
+        }else {
+            for (int i = 0; i < str.length(); i++) {
+              permutation(prefix + str.charAt(i), str.substring(0,i) + str.substring(i + 1, str.length()));
+            }
+
+        }
+
+    }
+
 }
