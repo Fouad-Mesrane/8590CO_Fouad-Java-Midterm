a93f8e0b60dad02d4dcf5ac634f3ac352e572ffc
Fouad-Mesrane <fouadmesrane@gmail.com>
2022-10-10 17:06:02 -0600

    10/10/22 math_problems.Fibonacci Sloved

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -2,13 +2,36 @@ package math_problems;
 
 public class Fibonacci {
 
-    /** INSTRUCTIONS
-     *
+    /**
+     * INSTRUCTIONS
+     * <p>
      * Write a method that will print or return at least 40 Fibonacci number
      * e.g. - 0,1,1,2,3,5,8,13
      **/
 
     public static void main(String[] args) {
 
+        fibonnaci(40);
+
     }
+
+    static void fibonnaci(int num) {
+        int num1 = 0, num2 = 1;
+
+        int counter = 0;
+
+        // Iterate till counter is num
+        while (counter < num) {
+
+            // Print the number
+            System.out.print(num1 + " ");
+
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            counter = counter + 1;
+        }
+    }
+
 }
