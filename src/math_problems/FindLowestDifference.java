c80ddb9885b74783f6b578beef90e292e8a429a5
Fouad-Mesrane <fouadmesrane@gmail.com>
2022-10-10 22:16:23 -0600

    10/10/22 math_problems.FindLowesDifference

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -1,8 +1,11 @@
 package math_problems;
 
+import java.util.*;
+
 public class FindLowestDifference {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to return the lowest number, that is not shared between the 2 arrays below
      * HINT: The lowest number that isn't shared between these arrays is 1
      */
@@ -13,8 +16,46 @@ public class FindLowestDifference {
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
 
+        lowestDifference(array1,array2);
     }
 
 
+    static void lowestDifference(int[] array1, int[] array2) {
 
+        // first convert to arrayList
+
+
+        List<Integer> arrayList1 = new ArrayList<>();
+        List<Integer> arrayList2 = new ArrayList<>();
+        for (int a : array1
+        ) {
+            arrayList1.add(a);
+        }
+        for (int b : array2
+        ) {
+            arrayList2.add(b);
+        }
+        // make a union
+        List<Integer> union = new ArrayList<>(arrayList1);
+        union.addAll(arrayList2);
+
+        // make an intersection
+        List<Integer> intersection = new ArrayList<>(arrayList1);
+        intersection.retainAll(arrayList2);
+
+        // remove duplicate of intersection from union
+        union.removeAll(intersection);
+
+
+        // sorting the array
+
+        Collections.sort(union);
+        // get the first index
+        System.out.println(union.get(0));
+
+    }
 }
+
+
+
+
