071e45d69fc4c98940dab3ffd0a3487099ec3313
Fouad-Mesrane <fouadmesrane@gmail.com>
2022-10-11 21:15:08 -0600

    11/10/22 data_structure.UseQueue Done

--- src/data_structures/UseQueue.java
+++ src/data_structures/UseQueue.java
@@ -1,5 +1,10 @@
 package data_structures;
 
+import databases.SharedStepsDatabase;
+
+import java.sql.SQLException;
+import java.util.*;
+
 public class UseQueue {
 
     /** INSTRUCTIONS
@@ -12,6 +17,53 @@ public class UseQueue {
 
 
     public static void main(String[] args) {
+        // creating a queue
+        Queue<Integer> queue = new PriorityQueue<>();
+        // adding values to a queue
+        queue.offer(2);
+        queue.offer(4);
+        queue.offer(52);
+        queue.offer(12);
+        queue.offer(63);
+        queue.offer(6);
+
+        // peek at the first element out
+        System.out.println(queue.peek());
+
+        // remove an element
+        System.out.println(queue.poll());
+        // iterate using for each
+        System.out.println("*************");
+        for (int num: queue
+             ) {
+            System.out.println(num);
+        }
+
+        // Iterate using Iterator
+
+        Iterator it = queue.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+
+        }
+        //copy queue data into an arrayList
+        List list = new ArrayList<>(queue);
+
+
+        // Connecting to DB
+        SharedStepsDatabase sql = new SharedStepsDatabase();
+        // creating a new table and store data in db
+        String tableName = "queueDB";
+        String colName = "queueValues";
+        sql.insertList(tableName,colName,list);
+
+        // retrieving data from DB
+        try {
+            List l = sql.executeQueryReadAll("select * from " + tableName);
+            System.out.println(l);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
