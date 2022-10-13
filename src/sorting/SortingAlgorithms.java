c8a94ede248abcb10913e9f10c6880af6fda1a07
Fouad-Mesrane <fouadmesrane@gmail.com>
2022-10-13 09:48:39 -0600

    10/13/22 sorting.SortingAlgorithms insertion_sort completed

--- src/sorting/SortingAlgorithms.java
+++ src/sorting/SortingAlgorithms.java
@@ -43,6 +43,17 @@ public class SortingAlgorithms {
     public int[] insertionSort(int[] array) {
         final long startTime = System.currentTimeMillis();
         // IMPLEMENT HERE
+        for (int i = 1; i < array.length; i++) {
+            int temp = array[i];
+            int j = i -1;
+
+            while (j >=0 && array[j] > temp){
+                array[j+1] = array[j];
+                j = j -1;
+            }
+            array[j+1] = temp;
+
+        }
 
         final long endTime = System.currentTimeMillis();
         this.executionTime = endTime - startTime;
